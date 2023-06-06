2d1f2524619b59f03b6014f5d6ee075d9d1a8dac
Usachev13 <2893318@gmail.com>
2023-06-06 17:59:34 +0300

    task_1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
+    public static void returnLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Год является високосным");
+        } else
+            System.out.println("Год не является високосным");
+    }
+
     public static void main(String[] args) {
         System.out.println("HW11 starts!");
+        System.out.println("Task 1");
+        int year = 2024;
+        returnLeapYear(year);
     }
 }
